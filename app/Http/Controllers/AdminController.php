8f78996eaefb97c4ae38080198036331e807f426
munene02 <munene02@gmail.com>
2018-01-22 05:11:25 +0300

    updated files

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -132,6 +132,7 @@ class AdminController extends Controller
     {
         $products = Product::with('size', 'group')->where([['group_id','=',$request->id],['status','=','no']])->get();
 
+        return $products;
         //return $request->id;
 
         return view('ProductVariantList', compact('products'));
